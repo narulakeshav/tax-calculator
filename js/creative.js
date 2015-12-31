7428736c449905479f0df27487c7fb93a194dfa6
Keshav Narula <NarulaKeshav13@gmail.com>
2015-12-30 23:53:25 -0800

    Update creative.js with Validations and Comments

--- js/creative.js
+++ js/creative.js
@@ -1,13 +1,23 @@
 $(document).ready(function() {
 	$('.btn-calculate').click(function() {
+
+		//Grab the value of the element in HTML file by id 
 		var price = document.getElementById("price").value;
+		var tax = document.getElementById("tax").value;
+
+		//Validate to see if value of price or tax is empty
+		if(price === "" || tax === "") {
+			alert("Please enter value for the input(s).")
+		}
+
+		//Change price to decimal and removes "$"
 		price = parseFloat(price.replace("$", ""));
 
-		var tax = document.getElementById("tax").value;
+		//Turn percent into decimal and removes the "%"
 		tax = parseFloat(tax.replace("%", "")) / 100;
 
+		//Round total to two decimal places and display in '.amount'
 		var total = (price + (price * tax)).toFixed(2);
-
 		$('.amount').html("$" + total);
 	});
 });
