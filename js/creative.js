41f9ffae6288e3b0bfcc89b926c61f686adf103f
Keshav Narula <NarulaKeshav13@gmail.com>
2015-12-31 00:05:38 -0800

    Add Validation case for string

--- js/creative.js
+++ js/creative.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
 	$('.btn-calculate').click(function() {
-
 		//Grab the value of the element in HTML file by id 
 		var price = document.getElementById("price").value;
 		var tax = document.getElementById("tax").value;
 
 		//Validate to see if value of price or tax is empty
 		if(price === "" || tax === "") {
-			alert("Please enter value for the input(s).")
+			alert("Please enter value for the input(s).");
+		}
+
+		//Validate to see if user entered string as input
+		else if(typeof price === "string" || typeof tax === "string") {
+			alert("Please enter valid inputs. Don't break my code!");
 		}
 
 		//Change price to decimal and removes "$"
